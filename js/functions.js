750f9ee481acd6ebfaaebf0e418123003c4c9c5f
username1298 <79992170040@ya.ru>
2023-06-30 11:10:58 +0300

    Выполняет задание 2.29

---
+++ js/functions.js
@@ -0,0 +1,39 @@
+// Функция для проверки длины строки.
+function lengthCheck (string, length) {
+  let stringProtector = String(string);
+  if (stringProtector.length <= length) {
+    return true;
+  }
+  return false;
+}
+
+// Функция для проверки, является ли строка палиндромом.
+function palindrome (string) {
+  let clearString = string.replaceAll(' ', '');
+  let lowerString = clearString.toLowerCase();
+  let newString = '';
+  for (let stringCycle = lowerString.length - 1; stringCycle >= 0 ; stringCycle-- ) {
+    newString += lowerString[stringCycle];
+  }
+  if (newString === lowerString) {
+    return true;
+  }
+  return false;
+}
+
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+function numberExtractor (string) {
+  let stringProtector = string.toString();
+  let numberString = '';
+  for (let stringBrute = 0; stringBrute <= stringProtector.length - 1; stringBrute++) {
+    let symbolCheck = parseInt(stringProtector[stringBrute], 10);
+    if (!isNaN(symbolCheck)) {
+      numberString += stringProtector[stringBrute];
+    }
+  }
+  if (numberString === '') {
+    return NaN;
+} else {
+    return numberString;
+}
+}
