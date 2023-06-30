b5525bb79976c90336603e39da3414659d991928
username1298 <79992170040@ya.ru>
2023-06-30 11:47:22 +0300

    Вносит правки линтера

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 // Функция для проверки длины строки.
 function lengthCheck (string, length) {
-  let stringProtector = String(string);
+  const stringProtector = String(string);
   if (stringProtector.length <= length) {
     return true;
   }
@@ -9,8 +9,8 @@ function lengthCheck (string, length) {
 
 // Функция для проверки, является ли строка палиндромом.
 function palindrome (string) {
-  let clearString = string.replaceAll(' ', '');
-  let lowerString = clearString.toLowerCase();
+  const clearString = string.replaceAll(' ', '');
+  const lowerString = clearString.toLowerCase();
   let newString = '';
   for (let stringCycle = lowerString.length - 1; stringCycle >= 0 ; stringCycle-- ) {
     newString += lowerString[stringCycle];
@@ -23,17 +23,17 @@ function palindrome (string) {
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 function numberExtractor (string) {
-  let stringProtector = string.toString();
+  const stringProtector = string.toString();
   let numberString = '';
   for (let stringBrute = 0; stringBrute <= stringProtector.length - 1; stringBrute++) {
-    let symbolCheck = parseInt(stringProtector[stringBrute], 10);
+    const symbolCheck = parseInt(stringProtector[stringBrute], 10);
     if (!isNaN(symbolCheck)) {
       numberString += stringProtector[stringBrute];
     }
   }
   if (numberString === '') {
     return NaN;
-} else {
+  } else {
     return numberString;
-}
+  }
 }
