2d2089d95f0181097e4684c741576240096b78a0
username1298 <79992170040@ya.ru>
2023-07-18 20:05:18 +0300

    Вносит правки линтера.

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ const getRandomInteger = (min, max) => {
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
 // Функция получения случайного не повторяющегося числа из диапозона
 const createRandomIdFromRangeGenerator = (min, max) => {
@@ -81,15 +81,15 @@ const generateCommentId = createRandomIdFromRangeGenerator(1, 750);
 // Функция получения массива - описание фотографии
 const photoDescription = function () {
 
-  const randomDescription = getRandomInteger(0, DESCRIPTIONS.length-1);
+  const randomDescription = getRandomInteger(0, DESCRIPTIONS.length - 1);
   const randomLikes = getRandomInteger(15, 200);
   const randomCommentsNumber = getRandomInteger(0, 30);
 
   // Получение массива comments
   const similarComments = function () {
-    const randomComment = getRandomInteger(0, COMMENTS.length-1);
+    const randomComment = getRandomInteger(0, COMMENTS.length - 1);
     const randomAvatar = getRandomInteger(1, 6);
-    const randomName = getRandomInteger(0, NAMES.length-1);
+    const randomName = getRandomInteger(0, NAMES.length - 1);
     return {
       id: generateCommentId(),
       avatar: `img/avatar-${randomAvatar}.svg`,
@@ -111,5 +111,7 @@ const photoDescription = function () {
 
 
 const similarDescriptions = Array.from({length: 25}, photoDescription);
-similarDescriptions();
+
+similarDescriptions;
+
 // Генерация данных: console.log(similarDescriptions);
